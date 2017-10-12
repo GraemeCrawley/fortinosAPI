3d4a58891eca9a52fb8df0a2509f71542e89da4c
graemecrawley <gacrawley@gmail.com>
2017-10-11 21:09:34 -0400

    Fixed file system

--- fortinosScraper/resetDatabase.py
+++ fortinosScraper/resetDatabase.py
@@ -8,17 +8,6 @@ import string
 import unicodedata
 
 
-def create_database(cursor):
-    try:
-        cursor.execute(
-        	"DROP DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
-        cursor.execute(    
-            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
-    except mysql.connector.Error as err:
-        print("Failed with database: {}".format(err))
-        exit(1)
-
-
 DB_NAME = raw_input('Please enter your database to reset: ')
 pw = raw_input('Please enter your password: ')
 
@@ -26,10 +15,23 @@ cnx = mysql.connector.connect(user='root', password=pw, database='FORTINOS')
 
 cursor = cnx.cursor(buffered=True)
 
+def create_database(cursor):
+    try:
+        cursor.execute(    
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+        print "Created database " + DB_NAME
+    except mysql.connector.Error as err:
+        print("Failed create database: {}".format(err))
+        exit(1)
 
+def drop_database(cursor):
+    cursor.execute(
+            "DROP DATABASE {}".format(DB_NAME))
+    print "Dropped database " + DB_NAME
 
 try:
-        cnx.database = DB_NAME 
+        drop_database(cursor)
+        cnx.database = DB_NAME
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_BAD_DB_ERROR:
         create_database(cursor)
